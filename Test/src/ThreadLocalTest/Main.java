91478cb99d46d4b0500320176dfe7030647c3b30
tang <466402070@qq.com>
2021-07-05 14:11:25 +0800

    21/07/05  提交threadlocal 实例

--- Test/src/ThreadLocalTest/Main.java
+++ Test/src/ThreadLocalTest/Main.java
@@ -1,7 +1,25 @@
 package ThreadLocalTest;
 
 public class Main {
-    public static void main(String[] args) {
 
+    static ThreadLocal<String> local = new ThreadLocal<>();
+
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                local.set("123");
+                String s = local.get();
+                System.out.println(s);
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String s = local.get();
+                System.out.println(s);
+            }
+        }).start();
     }
 }
