89ff98b7b0b90899fefe0b00cc3e23173d7befd1
tang <466402070@qq.com>
2021-07-05 14:08:42 +0800

    21/07/05  提交threadlocal 实例

--- Test1/src/main/java/ThreadLocalTest/Main.java
+++ Test1/src/main/java/ThreadLocalTest/Main.java
@@ -1,4 +1,24 @@
 package ThreadLocalTest;
 
+// 多个线程使用ThreadLocal类型的共享变量，解析见笔记
 public class Main {
+    static ThreadLocal<String> local = new ThreadLocal<>();
+
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                local.set("1243");
+                local.remove();
+                System.out.println(local.get());
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(local.get());
+            }
+        }).start();
+    }
 }
